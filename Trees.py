5ff463134f8f2ffff86dfa5655ed9e0b9575c435
Acusick1 <andrewcusick13@outlook.com>
2022-01-03 18:56:07 +0000

    Added tree inversion with test cases

--- Trees.py
+++ Trees.py
@@ -16,15 +16,15 @@ class Node:
         already been reached.
         Example input: [1, None, 3, None, None, 4, 5]
         """
-        v = vals.pop(0)
-        if v:
-            root = Node(v)
+
+        if vals:
+            root = Node(vals.pop(0))
             level = [root]
         else:
             return None
 
         while vals:
-            
+
             next_level = []
             for el in level:
                 left = vals.pop(0)
@@ -38,10 +38,31 @@ class Node:
                     el.right = Node(right)
 
                 next_level.append(el.right)
-                
+
             level = next_level
 
         return root
+
+    def to_list(self) -> List[int]:
+        """Breadth first traversal of binary tree, returns list of node values"""
+        # TODO: Include None values in list?
+
+        level = [self] if self else []
+        values = []
+
+        while level:
+            children = []
+            for el in level:
+                # Grab node value, add children to list (if present)
+                values.append(el.val)
+                if el.left:
+                    children.append(el.left)
+                if el.right:
+                    children.append(el.right)
+
+            level = children
+
+        return values
 
     def insert(self, data, left=True):
         """Insert a node to the left or right of a leaf node"""
@@ -120,11 +141,30 @@ class Solution:
 
         return depth
 
-        def invert_tree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-            pass
+    @staticmethod
+    def invert_tree(root: Optional[Node]) -> Optional[Node]:
+        """Invert binary tree; swap left and right values, retain and return root node"""
+        level = [root] if root else []
+
+        while level:
+            children = []
+            for i in range(len(level)):
+
+                if i < len(level) // 2:
+                    level[i].val, level[-i-1].val = level[-i-1].val, level[i].val
+
+                if level[i].left:
+                    children.append(level[i].left)
+
+                if level[i].right:
+                    children.append(level[i].right)
+
+            level = children
+
+        return root
+
 
 def example_tree():
-
     vals = [3, 9, 20, 15, 7]
     nodes = []
 
@@ -144,6 +184,15 @@ class TestTress(unittest.TestCase):
 
     def setUp(self):
         self.solution = Solution()
+
+    def test_to_list(self):
+        data = [([1, 2, 3, 4, None, 6, 7], [1, 2, 3, 4, 6, 7])]
+
+        i = 0
+        for inp, out in data:
+            root = Node.from_list(inp)
+            with self.subTest(i=i):
+                self.assertEqual(root.to_list(), out)
 
     def test_traverse_inorder(self):
         data = [([3, 9, 20, None, None, 15, 7], [9, 3, 15, 20, 7])]
@@ -182,7 +231,19 @@ class TestTress(unittest.TestCase):
                 self.assertEqual(self.solution.max_depth(root), out)
             i += 1
 
+    def test_invert_tree(self):
+        data = [([4, 2, 7, 1, 3, 6, 9], [4, 7, 2, 9, 6, 3, 1]),
+                ([2, 1, 3], [2, 3, 1]),
+                ([], [])]
+        i = 0
+        for inp, out in data:
+            root = Node.from_list(inp)
+            res = self.solution.invert_tree(root)
+            res = res.to_list() if res else []
+            with self.subTest(i=i):
+                self.assertEqual(res, out)
+            i += 1
+
 
 if __name__ == "__main__":
-
     unittest.main(verbosity=2)
